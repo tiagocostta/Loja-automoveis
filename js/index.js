81dd995fb3bc927d3d87ed74a9e8ee68294f7815
GetulioLT <getulio.santos.lesstime@gmail.com>
2023-06-20 15:14:39 -0300

    0.2.1
    Adição de comentários.

--- js/index.js
+++ js/index.js
@@ -5,23 +5,29 @@ fetch("php/alocacao.php")
     .then(function (data) {
         // Manipula os dados recebidos do PHP
         console.log(data);
+
+        // array das areas disponiveis sem as outras colunas
         areas = data.map(({ area }) => parseInt(area))
-        console.log()
-        // console.log(areas.includes(4))
-        // Aqui você pode fazer qualquer manipulação necessária nos dados
+
+        // Variavel responsavel por armazenar o codigo html
         let codHtml = ""
+
+        // Estrutura para a verificação de area de 1 a 6
         for (let i = 1; i <= 6; i++) {
+            
+            // Condição para verificar se o numero existe dentro do array
             if (areas.includes(i)) {
                 codHtml += `<div class="area${i} border" onclick="area(${i})" style="background-color: blue; color: white;">${i}</div>`
-
             } else {
                 codHtml += `<div class="area${i} border" onclick="aviso()" style="background-color: white">${i}</div>`
             }
         }
+
+        // Local que é responsavel por substituir no html
         document.getElementById("principal").innerHTML = codHtml
     })
     .catch(function (error) {
-        alert("aaaaaaa");
+        alert(error);
     });
 
 function aviso() {
