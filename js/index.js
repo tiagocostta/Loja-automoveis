46cce1616c17ba0221c051e5d9720c2429dbf8d2
GetulioLT <getulio.santos.lesstime@gmail.com>
2023-06-20 15:05:40 -0300

    0.2
    primeira pag totalmente funcional e operante, responsiva de acordo com o banco de dados

--- js/index.js
+++ js/index.js
@@ -1,19 +1,34 @@
-function aviso(){
-    //alert("Area");
-    window.location.href = "teste.html";
+fetch("php/alocacao.php")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        // Manipula os dados recebidos do PHP
+        console.log(data);
+        areas = data.map(({ area }) => parseInt(area))
+        console.log()
+        // console.log(areas.includes(4))
+        // Aqui você pode fazer qualquer manipulação necessária nos dados
+        let codHtml = ""
+        for (let i = 1; i <= 6; i++) {
+            if (areas.includes(i)) {
+                codHtml += `<div class="area${i} border" onclick="area(${i})" style="background-color: blue; color: white;">${i}</div>`
+
+            } else {
+                codHtml += `<div class="area${i} border" onclick="aviso()" style="background-color: white">${i}</div>`
+            }
+        }
+        document.getElementById("principal").innerHTML = codHtml
+    })
+    .catch(function (error) {
+        alert("aaaaaaa");
+    });
+
+function aviso() {
+    alert("Area indisponivel para compra");
 }
 
-function area1(){
+function area(local) {
     window.location.href = "autos.html";
-    localStorage.setItem("titulo", "Area 1");
-}
-
-function area3(){
-    window.location.href = "autos.html";
-    localStorage.setItem("titulo", "Area 3");
-}
-
-function area4(){
-    window.location.href = "autos.html";
-    localStorage.setItem("titulo", "Area 4");
+    localStorage.setItem("titulo", "Area " + local);
 }
---
+++ php/automoveis.php
@@ -0,0 +1,38 @@
+<?php
+// Configuração do banco de dados
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "db_conce";
+
+// Cria uma conexão com o banco de dados
+$conn = new mysqli($host, $username, $password, $database);
+
+// Verifica se ocorreu algum erro na conexão
+if ($conn->connect_error) {
+    die("Erro ao conectar ao banco de dados: " . $conn->connect_error);
+}
+
+// Consulta SQL para obter os dados do banco de dados
+$sql = "SELECT * FROM automoveis";
+$result = $conn->query($sql);
+
+// Verifica se a consulta retornou resultados
+if ($result->num_rows > 0) {
+    // Array para armazenar os resultados da consulta
+    $data = array();
+
+    // Itera sobre os resultados e adiciona-os ao array
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+
+    // Codifica os dados como JSON e envia para JavaScript
+    echo json_encode($data);
+} else {
+    echo "Nenhum resultado encontrado.";
+}
+
+// Fecha a conexão com o banco de dados
+$conn->close();
+?>
